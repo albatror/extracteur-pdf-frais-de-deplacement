91c8399f22e57aef8423f9354338066b6cf6e9fd
X0r3 <r3vzone@gmail.com>
2025-09-14 17:15:38 +0200

    Add next.config.ts 1757862938386

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+    ],
+  },
+}
+
+export default nextConfig
